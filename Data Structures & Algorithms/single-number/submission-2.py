468275ce07df6b3648f52450a2021823a182aed9
Dee <36694535+itsme008@users.noreply.github.com>
2026-06-08 20:40:13 +0300

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        #hashmap
+        # count = {}
+
+        # for i in nums:
+        #     count[i] = count.get(i, 0) +1
+
+        # for n in count:
+        #     if count[n] == 1:
+        #         return n
+
+        result = 0
+
+        for n in nums:
+            result ^= n
+
+        return result
